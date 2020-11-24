17ea8f9137b3a59bc2f4ea872815c20c348ca77c
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2020-11-24 12:31:04 +0100

    Introducida plataforma en game.js

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ export default class Game extends Phaser.Scene
     this.load.image('fondo1', 'https://dbrey.github.io/Proyecto-PVLI/Sprites/Background/Fondo_tras_del_todo.png');
     this.load.image('barril', 'https://dbrey.github.io/Proyecto-PVLI/Sprites/Obstáculos/Barril.png');
     this.load.spritesheet('playerSheet', 'https://dbrey.github.io/Proyecto-PVLI/Sprites/Chracters/SpritesheetCorrer.png', { frameWidth: 161, frameHeight: 216 });
+    this.load.image('plataforma', 'https://dbrey.github.io/Proyecto-PVLI/Sprites/Background/Plataforma.png');
     // CAMBIAR VALORES DEL SPRITESHEET
     //El 4 y el 37 son las dimensiones de cada frame por separado (4x37), y el 234 es la CANTIDAD de frames que hay en el spriteSheet
     
@@ -27,7 +28,8 @@ export default class Game extends Phaser.Scene
 
 
 
-
+    var plataformaSuelo = this.add.sprite(300,200, 'plataforma');
+    plataformaSuelo.setScale(1);
     var fondoImg = this.add.sprite(700, 400, 'fondo1');
     fondoImg.setScale(1.7);
     var calimoImg = this.add.sprite(1100,210, 'botellaCalimocho');

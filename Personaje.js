382f97fc1ec1ba20605b18d80d051cf6cf8c7507
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-12-02 20:29:09 +0100

    otros cambios de movimiento

--- Personaje.js
+++ Personaje.js
@@ -1,31 +1,29 @@
-export default class Personaje extends Phaser.Physics.Arcade.Sprite 
+class Personaje extends Phaser.GameObjects.Sprite 
 {
   constructor(scene, x, y,anim) {
 
-    // let aspecto = scene.physics.add.sprite(0, 0, 'playerSheet');
     super(scene,x,y,anim);
 
-    scene.add.existing(this);
     scene.physics.world.enable(this);
     this.setColliderWorldBounds(true);
 
-    // scene.add.existing(this);
+    this.scene.add.existing(this);
 
-    // scene.physics.world.enableBody(this);
-    // this.body.velocity.x = 100;
-    // this.setVelocityX(160);
-
-    // scene.Personaje.add(this);
+    this.scene.physics.world.enableBody(this);
+    this.body.setColliderWorldBounds(true);
+    this.body.velocity.x = 100;
+    
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   preload()
   {
     this.load.spritesheet('playerSheet', 'https://dbrey.github.io/Proyecto-PVLI/Sprites/Characters/SpritesheetCorrer.png', { frameWidth: 161, frameHeight: 216 });
-  }
 
-  create()
-  {
-    this.player = this.physics.add.sprite(200,420, 'playerSheet');
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-50);
+    }
 
 
   }

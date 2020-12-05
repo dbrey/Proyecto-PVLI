13770696b6942ccc245651357e02943e1af0a10f
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2020-12-05 18:24:02 +0100

    Create personaje.js

---
+++ personaje.js
@@ -0,0 +1,19 @@
+export default class Personaje extends Phaser.GameObjects.Sprite 
+{
+  constructor(scene, x, y,anim) {
+
+    super(scene,x,y,anim);
+
+    this.scene.add.existing(this);
+
+    
+
+    this.scene.physics.world.enableBody(this);
+    this.body.setCollideWorldBounds();
+    //this.body.setVelocityX(100); Esto lo está usando Álvaro para probar. Por eso está comentado no lo borréis
+  }
+
+  preupdate(t, d){
+    super.preUpdate(t, d);
+  }
+}

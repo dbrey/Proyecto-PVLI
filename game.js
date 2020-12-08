42d861c24092e6868f881d0b9e5407ee85dae72c
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-12-08 18:53:22 +0100

    cambios tilemap

--- game.js
+++ game.js
@@ -17,8 +17,10 @@ export default class Game extends Phaser.Scene
     this.load.spritesheet('corrersheet', './sprites/characters/spritesheetcorrer.png', { frameWidth: 161, frameHeight: 216 });
     this.load.spritesheet('agacharsesheet', './sprites/characters/spritesheetagacharse.png', { frameWidth: 218, frameHeight: 218 })
     this.load.audio('mainsoundtrack', './sonidos/queviva.mp3');
-    this.load.image('citytileset','./sprites/tiles/citytileset.png');
-    this.load.image('rowhousetileset','./sprites/tiles/rowhousetileset.png');
+
+
+    this.load.image('city','./sprites/tiles/citytileset.png');
+    this.load.image('rowhouse','./sprites/tiles/rowhousetileset.png');
     this.load.tilemapTiledJSON('block1','./sprites/tiles/block1.json');
   }
 
@@ -61,19 +63,25 @@ export default class Game extends Phaser.Scene
     
 
     this.cameramain = this.cameras.main;
-// ------------------------------------------------------------------
-    // this.plataformasuelo = this.physics.add.staticGroup();
+// ------------------------  MAPA  ---------------------------------
 
-// ------------------------ FISICAS ---------------------------------
-    // this.plataformasuelo.create(100,490, 'plataforma');
-    // this.plataformasuelo.create(400,490, 'plataforma');
-    // this.plataformasuelo.create(700,490, 'plataforma');
-    // this.plataformasuelo.create(1000,490, 'plataforma');
-    // this.plataformasuelo.create(1300,490, 'plataforma');
+   this.map = this.make.tilemap({ 
+      key: 'block1', 
+      tileWidth: 64, 
+      tileHeight: 64 
+    });
 
+    const tileset1 = this.map.addTilesetImage('suelo', 'city');s
+
+    this.groundLayer =  this.map.createStaticLayer('suelo', [tileset1]);
+
+    // this.map = this.game.add.tilemap('block1');
+    // this.map.addTilesetImage('city');
+    // this.map.addTilesetImage('rowhouse');
     
-    // this.physics.add.collider(this.barrilimg, this.plataformasuelo);
-    // this.physics.add.collider(this.calimoimg, this.plataformasuelo);
+    // this.floor = this.map.createLayer('suelo');
+    // this.floor.resizeWorld();
+    //this.physics.add.collider(this.player, this.floor);
 // ------------------------------------------------------------------
   };
 

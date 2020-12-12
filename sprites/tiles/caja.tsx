829a7b477648eaebc3ab0880bfbf13f2c1bb1b59
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-12-12 12:40:31 +0100

    Mejora Bloque 1

---
+++ sprites/tiles/caja.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="caja" tilewidth="128" tileheight="128" tilecount="9" columns="3">
+ <image source="../../imagenes/obstaculos/caja.PNG" trans="ff00ff" width="488" height="488"/>
+</tileset>

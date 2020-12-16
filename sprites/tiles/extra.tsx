bca8eda0e1e9926088e0458cf4797631c19e7a1f
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2020-12-16 23:07:52 +0100

    Bloque 1 final
    Faltan los objetos

--- sprites/tiles/extra.tsx
+++ sprites/tiles/extra.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="extra" tilewidth="32" tileheight="32" tilecount="640" columns="20">
- <image source="devil'scointileset.png" trans="ff00ff" width="640" height="1024"/>
+ <image source="devil.png" trans="ff00ff" width="640" height="1024"/>
 </tileset>

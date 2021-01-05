e1e26c151a9c6b3aaa87e32d7db6da1a839e2d86
ARodulfo <72988547+ARodulfo@users.noreply.github.com>
2021-01-05 01:46:42 +0100

    Jagger y calimocho added

---
+++ calimocho.js
@@ -0,0 +1,34 @@
+export default class Calimocho extends Phaser.GameObjects.Sprite 
+{
+    constructor(scene, x, y){
+        super(scene, x, y,'calimocho');
+        this.scene.add.existing(this);
+        this.delay_time = 2000;
+        //this.scene.physics.add.staticImage(x, y,'botella_agua');
+        this.scene.physics.add.existing(this, true);
+        this.setScale(0.05);
+        this.body.setSize(20, 65);
+        this.body.setOffset(155, 610);
+    }
+
+    preUpdate(t,d){
+        super.preUpdate(t, d);
+        
+        if(this.scene.physics.overlap(this, this.scene.player)){
+            this.barraalcohol_gone();
+        }
+    }
+    barraalcohol_gone(){
+        this.scene.alcohol.actualizar_barra = false;
+        this.scene.alcohol.images.setFrame(this.scene.alcohol.frames_alcohol[15]);
+        this.y = 3000;
+        this.body.setOffset(155, 1000);
+        this.timedEvent = this.scene.time.delayedCall(this.delay_time, this.onEvent, [], this);
+        console.log("calimocho");
+    }
+
+    onEvent(){
+        this.scene.alcohol.actualizar_barra = true;
+        this.destroy();
+    }
+}
---
+++ jagger.js
@@ -0,0 +1,25 @@
+export default class Jagger extends Phaser.GameObjects.Sprite 
+{
+    constructor(scene, x, y){
+        super(scene, x, y,'jagger');
+        this.scene.add.existing(this);
+        //this.scene.physics.add.staticImage(x, y,'botella_agua');
+        this.scene.physics.add.existing(this, true);
+        this.setScale(0.05);
+        this.body.setSize(25, 60);
+        this.body.setOffset(210, 540);
+    }
+
+    preUpdate(t,d){
+        super.preUpdate(t, d);
+        
+        if(this.scene.physics.overlap(this, this.scene.player)){
+            this.alcohol_maximo();
+        }
+    }
+    alcohol_maximo(){
+        this.scene.alcohol.aumentar_ebriedad(this.scene.alcohol.max_alcohol);
+        this.destroy();
+        console.log("jagger");
+    }
+}

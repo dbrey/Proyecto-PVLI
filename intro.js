1b3309d7e3fe01bd95960aec19cc53ace8a1361a
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2021-01-08 19:06:31 +0100

    intro funciona

--- intro.js
+++ intro.js
@@ -7,14 +7,21 @@ export default class Intro extends Phaser.Scene
 
   preload() {
     this.load.video('intro','./imagenes/intro.mp4');
-  }
+  }te
 
   create()
   {
-    this.add.video(700,400,'intro');
+    this.video = this.add.video(700,400,'intro');
+    this.video.setScale(2.4);
+    this.video.play(true);
   }
   update() {
     console.log(this.video.getProgress()); // por donde va en el video
+
+    if(this.video.getProgress() >= 0.99)
+    {
+      this.scene.start('menu');
+    }
   };
 
 }

e124924510e08c411dd19052559b7977b93c1b58
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2021-01-15 03:31:26 +0100

    escena creditos

--- CreditsScene.js
+++ CreditsScene.js
@@ -11,14 +11,14 @@ export default class CreditsScene extends Phaser.Scene
   }
 
   preload() {
-    this.load.image('credits', './sprites/menu_inicial/bar_atlas.png');
+    this.load.image('credits', './sprites/escena_creditos/creditos.png');
+    this.load.image('fondo', './sprites/escena_creditos/fondo_creditos.png');
   }
 
 
   create() {
-
-    this.fondo = this.add.sprite(700, 0, 'credits');
-    this.fondo.setScale(1.5);
+    this.fondo = this.add.sprite(700, 400, 'fondo');
+    this.creditos = this.add.sprite(700, 1000, 'credits');
     //boton
     let menu = this.add.image(150,700, 'botonplaynormal').setInteractive();
 
@@ -30,9 +30,9 @@ export default class CreditsScene extends Phaser.Scene
   };
   update() //Supongo que la imagen final tiene de tamaño 1000
   {
-    if(this.fondo.y < 1000)
+    if(this.creditos.y > -200)
     {
-      this.fondo.y++;
+      this.creditos.y -= 0.6;
     }
     if(this.enter.isDown)
     {

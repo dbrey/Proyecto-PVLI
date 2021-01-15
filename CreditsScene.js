ef1139af76d9365d032229245cfb6cdbd88320bf
PabloGlezAlv <62878159+PabloGlezAlv@users.noreply.github.com>
2021-01-15 03:51:00 +0100

    boton creditos

--- CreditsScene.js
+++ CreditsScene.js
@@ -13,17 +13,29 @@ export default class CreditsScene extends Phaser.Scene
   preload() {
     this.load.image('credits', './sprites/escena_creditos/creditos.png');
     this.load.image('fondo', './sprites/escena_creditos/fondo_creditos.png');
+
+    this.load.image('menuoff', './sprites/escena_creditos/main1.png');
+    this.load.image('menuon', './sprites/escena_creditos/main2.png');
   }
 
 
   create() {
     this.fondo = this.add.sprite(700, 400, 'fondo');
     this.fondo.setDepth(2);
-    this.creditos = this.add.sprite(700, 1000, 'credits');
+    this.creditos = this.add.sprite(700, 1130, 'credits');
     //boton
-    let menu = this.add.image(150,700, 'botonplaynormal').setInteractive();
-
+    let menu = this.add.image(150,700, 'menuoff').setInteractive();
+    menu.setDepth(3);
     this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+    menu.on('pointerover', event => 
+    {
+      menu = this.add.image(150,700, 'menuoff');
+    });
+    menu.on('pointerout', event => 
+    {
+      menu = this.add.image(150,700, 'menuon');
+    });
 
     menu.on('pointerdown', event => {
       this.volvermenu();
@@ -33,7 +45,7 @@ export default class CreditsScene extends Phaser.Scene
   {
     if(this.creditos.y > -200)
     {
-      this.creditos.y -= 0.6;
+      this.creditos.y -= 0.7;
     }
     if(this.enter.isDown)
     {

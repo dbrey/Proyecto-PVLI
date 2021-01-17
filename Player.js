a0532dc0d01ca27d1453524ba06a915b3f914a46
David Brey <dbrey@ucm.es>
2021-01-17 18:39:49 +0100

    Retroceso parcheado y pisar obsmov roto

--- Player.js
+++ Player.js
@@ -188,14 +188,33 @@ export default class Player extends Personaje
 
   }
 
-  ralentizar(dureza)
+  ralentizar(dureza, nombre)
   {
     //this.speed -= dureza //Por alguna razon, si se pone este codigo, el jugador se destruye
-    
-    this.speed -= 200
-    if(this.speed < -200)
+    this.retroceso;
+    this.limite = 400;
+    if(nombre === "caja" || nombre === "barril")
     {
-      this.speed = -200;
+      this.retroceso = 200;
+    }
+    else if(nombre === "botellavacia" || nombre === "jarron")
+    {
+      this.retroceso = 100;
+    }
+    else if(nombre === "coche" || nombre === "cocheoscuro")
+    {
+      this.retroceso = 1000;
+      this.limite = 1000;
+    }
+    else if (nombre === "barriltop")
+    {
+      this.retroceso = 300;
+    }
+
+    this.speed -= this.retroceso;
+    if(this.speed < -this.limite)
+    {
+      this.speed = -this.limite;
     }    
   }
 }
